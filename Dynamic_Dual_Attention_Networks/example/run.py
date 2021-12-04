17681b351514cc0b9ebdcdbf3b9e93ad3b14543c
yerong <yerong.li@outlook.com>
2021-12-03 19:31:30 -0600

    Tensor +1

--- Dynamic_Dual_Attention_Networks/example/run.py
+++ Dynamic_Dual_Attention_Networks/example/run.py
@@ -14,11 +14,14 @@ def run(rank, size):
         # Send the tensor to process 1
         print('Rank 0 started sending')
         dist.send(tensor=tensor, dst=1)
-    else:
+    else if rank == 1:
         # Receive tensor from process 0
         print('Rank 1 started receiving')
         dist.recv(tensor=tensor, src=0)
         tensor += 1
+    else:
+        print('Rank 2 started receiving')
+        dist.recv(tensor=tensor, src=0)
     print('Rank ', rank, ' has data ', tensor[0])
 
 def init_process(rank, size, fn, backend='gloo'):

b65cb02141bf8c3940ed24fc3c026d9d13ec183e
yerong <yerong.li@outlook.com>
2021-12-03 19:40:21 -0600

    all_reduce

--- Dynamic_Dual_Attention_Networks/example/run.py
+++ Dynamic_Dual_Attention_Networks/example/run.py
@@ -6,25 +6,30 @@ import torch.distributed as dist
 import torch.multiprocessing as mp
 
 """Blocking point-to-point communication."""
-
 def run(rank, size):
-    tensor = torch.zeros(1)
-    if rank == 0:
-        tensor += 1
-        # Send the tensor to process 1
-        print('Rank 0 started sending')
-        dist.send(tensor=tensor, dst=1)
-        tensor -=2
-        dist.send(tensor=tensor, dst=2)
-    elif rank == 1:
-        # Receive tensor from process 0
-        print('Rank 1 started receiving')
-        dist.recv(tensor=tensor, src=0)
-        tensor += 1
-    else:
-        print('Rank 2 started receiving')
-        dist.recv(tensor=tensor, src=0)
+    """ Simple collective communication. """
+    group = dist.new_group([0, 1])
+    tensor = torch.ones(1)
+    dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
     print('Rank ', rank, ' has data ', tensor[0])
+# def run(rank, size):
+#     tensor = torch.zeros(1)
+#     if rank == 0:
+#         tensor += 1
+#         # Send the tensor to process 1
+#         print('Rank 0 started sending')
+#         dist.send(tensor=tensor, dst=1)
+#         tensor -=2
+#         dist.send(tensor=tensor, dst=2)
+#     elif rank == 1:
+#         # Receive tensor from process 0
+#         print('Rank 1 started receiving')
+#         dist.recv(tensor=tensor, src=0)
+#         tensor += 1
+#     else:
+#         print('Rank 2 started receiving')
+#         dist.recv(tensor=tensor, src=0)
+#     print('Rank ', rank, ' has data ', tensor[0])
 
 def init_process(rank, size, fn, backend='gloo'):
     """ Initialize the distributed environment. """

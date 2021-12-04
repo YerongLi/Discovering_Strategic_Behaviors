0d07fd40f43b061ec8427e01b3384236b3404180
yerong <yerong.li@outlook.com>
2021-12-04 03:49:41 -0600

    Add coloring the print the lengths of the dataset

--- reproduce/Model/Dataset.py
+++ reproduce/Model/Dataset.py
@@ -4,7 +4,7 @@ import numpy as np
 from utils import *
 import torch
 
-
+LIMIT_DATA = 1000
 class DBLPDataset:
     
     
@@ -23,6 +23,10 @@ class DBLPDataset:
     def __read__(self, c_batchsize, a_batchsize):
         
         c_active, c_position, ca_adj, c_emb, c_edgellh = pickle.load(open(f'{self.path}/{self.strategy}_input/c_{self.strategy}_inputs_{self.year}.pkl', 'rb'))        
+        c_active = c_active[:LIMIT_DATA]
+        c_position = c_position[:LIMIT_DATA]
+        c_emb = c_emb[:LIMIT_DATA]
+        c_edgellh = c_edgellh[:LIMIT_DATA]
         print(tokgreen('lengths'))
         print(len(c_active), len(c_position), len(ca_adj), len(c_emb), len(c_edgellh))
         c_edgecount = np.array([len(edge) for edge in c_edgellh])

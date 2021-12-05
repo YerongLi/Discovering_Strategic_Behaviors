3f2b2383125e8c8d64739d1ee786ab161e55014a
yerong <yerong.li@outlook.com>
2021-12-05 17:08:52 -0600

    dimension

--- Codes/LR.py
+++ Codes/LR.py
@@ -44,7 +44,7 @@ def LR(stra):
         for author, llhs in exp_input:
                 
             X = torch.from_numpy(llhs/np.sum(llhs,axis=1).reshape(-1,1)).float().to('cpu')
-            print(tokgreen('X'))
+            print('X')
             print(X.shape)
             y = torch.from_numpy(np.ones(len(llhs))).float().to('cpu')
             print(y.shape)

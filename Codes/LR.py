dbf5199b9f4e59254d178d5b6acb6dbf449c79f0
yerong <yerong.li@outlook.com>
2021-12-05 15:25:35 -0600

    Test a_cite_inputs_2018.pkl

--- Codes/LR.py
+++ Codes/LR.py
@@ -38,7 +38,7 @@ def LR(stra):
         # exp_input = pickle.load(open(f'/home/yuxinx2/DBLP_exp/LR/{stra}/{year}_{stra}_input_{fold}.pkl','rb'))
 
         a_active, a_position, ac_adj, a_emb, da_emb, a_edgellh = pickle.load(open(f'{os.getenv("HOME")}/yerong/Discovering_Strategic_Behaviors/Dynamic_Dual_Attention_Networks/cite_input/a_cite_inputs_2018.pkl','rb'))
-        exp_input = a_position      
+        exp_input = zip(a_active, a_edgellh)      
         results = {}
         for author, llhs in exp_input.items():
                 
